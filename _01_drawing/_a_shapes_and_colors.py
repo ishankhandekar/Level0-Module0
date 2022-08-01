60fc59dae2d41babaaa43b7d258e8139caa191aa
ishankhandekar <66933296+ishankhandekar@users.noreply.github.com>
2022-08-01 11:58:33 -0700

    8/1/2022

--- _01_drawing/_a_shapes_and_colors.py
+++ _01_drawing/_a_shapes_and_colors.py
@@ -5,31 +5,41 @@ if __name__ == '__main__':
     window.bgcolor('white')
 
     # This code makes a new Turtle. Pick a new name for the turtle
-    my_turtle = turtle.Turtle()
+    blue = turtle.Turtle()
 
     # Make your turtle's shape 'turtle', .shape('turtle')
-
+    turtle.shape('turtle')
     # Set your turtle's speed using .speed(2)
-
+    turtle.speed(2)
     # Set your turtle's color using .color('green') and .pencolor('blue')
-
+    turtle.color('blue')
     # Move your turtle forward using .forward(100)
     # TEST    Did your turtle move forward?
-
+    
     # Move your turtle left or right using .left(90) or .right(90)
-
+    turtle.begin_fill()
+    for x in range(4):
+        turtle.left(90)
+        turtle.forward(100)
+    turtle.end_fill()
     # Now put the forward and left/right code into a for loop to repeat 4 times.
     # TEST    Did your turtle draw a square?
 
     # Move your turtle to a new place on the screen using .goto(x, y)
     # x=0 and y=0 is the center of the screen
-
+    turtle.penup()
+    turtle.goto(15,60)
+    
     # Have your turtle draw a circle using .circle(radius, steps=30)
+    turtle.pendown()
+    turtle.begin_fill()
+    turtle.circle(50, steps = 30)
+    turtle.end_fill()
     # TEST    Did your turtle draw a circle?
 
     # Add color to your shape by adding .begin_fill() before drawing the circle
     # and .end_fill() below
-
+   
     # Draw 3 more shapes with different fill colors!
 
     # ===================== DO NOT EDIT THE CODE BELOW ============================
--- _02_code_flow/_c_flaming_ninja_star.py
+++ _02_code_flow/_c_flaming_ninja_star.py
@@ -21,42 +21,43 @@ if __name__ == '__main__':
     flameSize = 130         # the length of the flaming arms
     
     # Make a new turtle
-    
+    turtle = turtle.Turtle()
     # Make the turtle shape 'turtle', .shape('turtle')
-    
+    turtle.shape('turtle')
     # Set the turtle width to 2
-    
+    turtle.width(2)
     # Set the turtle speed to 0 (fastest)
-    
+    turtle.speed(0)
     # Use a for loop to repeat all of the code below ONE time (we will change
     # this later)
-        
+    for i in range(25):
+        turtle.pencolor(get_random_color())
         # Set the turtle .fillcolor() to orange
-        
+        turtle.fillcolor('orange')
         # Call the turtle .begin_fill() function
-        
+        turtle.begin_fill()
         # TURN RIGHT     Turn the turtle 1/8 of a circle (hint: 360 degrees
         #                will turn a full circle)
-        
+        turtle.right(360/8)
         # DRAW           Move the turtle 64 pixels
-        
+        turtle.forward(64)
         # TURN LEFT      Turn the turtle 40 degrees to the LEFT. (Negative
         #                numbers will turn the turtle counter-clockwise.)
-        
+        turtle.left(40)
         # DRAW FLAME     Move the turtle the distance in the variable flameSize
-        
+        turtle.forward(flameSize)
         #                Turn the turtle to the right 170 degrees
-         
+        turtle.right(170)
         #                Move the turtle the distance in the variable flameSize (again)
-         
+        turtle.forward(flameSize)
         #  TURN RIGHT    Turn the turtle 62 degrees to the right
-        
+        turtle.right(62)
         #  DRAW          Move the turtle the distance in the variable baseSize
-        
+        turtle.forward(baseSize)
         # Call the turtle .end_fill() method
-        
+        turtle.end_fill()
     # Hide your turtle so you can see the pattern.
-        
+    turtle.hideturtle()
     # TEST   Run the program. Check that your shape is the same as the first
     #        picture in the recipe. This is one arm of the ninja star.
 
